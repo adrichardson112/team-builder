62e1ee69f23e1b27cbad674292a90a3ae67be4de
Ashley Richardson <ashley-richardson@lambdastudents.com>
2020-09-04 16:36:28 -0400

    fixed members

--- team-builder/src/Components/Form.js
+++ team-builder/src/Components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'React';
+import React, { useState} from 'react';
 
 const Form = (props) => {
     const [member, setMember] = useState({
--- team-builder/src/Components/Members.js
+++ team-builder/src/Components/Members.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const Member = props => {
+const Members = (props) => {
     return (
-        <div className="member-list">
+        <div className="members-list">
             {props.members.map((friend) => (
-                <div className="member" key={friend.id}>
-                    <h3>{friend.name}</h3>
-                    <p>Email: {friend.email}</p>
-                    <p>Role: {friend.role}</p>
-                    <p>Hobbies: {friend.hobbies}</p>
-                </div>
+                    <div className="members" key={friend.id}>
+                        <h3>{friend.name}</h3>
+                        <p>Email: {friend.email}</p>
+                        <p>Role: {friend.role}</p>
+                        <p>Hobbies: {friend.hobbies}</p>
+                    </div>
             ))}
         </div>
     );
 };
 
-export default Member;
+export default Members;
