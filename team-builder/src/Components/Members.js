eb4942027f98cbb72c1ea5ae9f1a262453b7a0f7
Ashley Richardson <ashley-richardson@lambdastudents.com>
2020-09-04 16:39:38 -0400

    edit

--- team-builder/src/Components/Members.js
+++ team-builder/src/Components/Members.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Members = (props) => {
+    console.log("Members props: ", props);
+    
     return (
         <div className="members-list">
             {props.members.map((friend) => (
